ea4c84ed7a475e6998fd9e3145cc729aea4a583e
resparx <ramprash.rspx@gmail.com>
2025-07-04 12:06:33 +0530

    fix: tailwind config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import Header from './components/Header'
-import Hero from './components/Hero'
-import Steps from './components/Steps'
-import Portfolio from './components/Portfolio'
-import Testimonial from './components/Testimonial'
-import FAQ from './components/FAQ'
-import Footer from './components/Footer'
+import Header from '../components/Header'
+import Hero from '../components/Hero'
+import Steps from '../components/Steps'
+import Portfolio from '../components/Portfolio'
+import Testimonial from '../components/Testimonial'
+import FAQ from '../components/FAQ'
+import Footer from '../components/Footer'
+
 
 export default function Home() {
   return (
