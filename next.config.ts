9aff286bf1da5816c4de7d7608817734d5d7d03c
karthiwayne <karthikeyanv2003@gmail.com>
2025-07-09 23:54:56 +0530

    next config back to first

--- next.config.ts
+++ next.config.ts
@@ -3,18 +3,6 @@ const nextConfig = {
   images: {
     domains: ['images.pexels.com'],
   },
-  // Disable SWC minification but allow transforms
-  swcMinify: false,
-  // Use webpack for faster builds in development
-  webpack: (config, { dev }) => {
-    if (dev) {
-      config.watchOptions = {
-        poll: 1000,
-        aggregateTimeout: 300,
-      }
-    }
-    return config
-  },
 }
 
 module.exports = nextConfig
